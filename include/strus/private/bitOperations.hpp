4629a941ec026e698fea5e84b63ceb050261255a
Andreas Baumann <mail@andreasbaumann.cc>
2016-01-23 08:51:25 +0100

    added posting list helpers from strus

---
+++ include/strus/private/bitOperations.hpp
@@ -0,0 +1,119 @@
+/*
+---------------------------------------------------------------------
+    The C++ library strus implements basic operations to build
+    a search engine for structured search on unstructured data.
+
+    Copyright (C) 2015 Patrick Frey
+
+    This library is free software; you can redistribute it and/or
+    modify it under the terms of the GNU General Public
+    License as published by the Free Software Foundation; either
+    version 3 of the License, or (at your option) any later version.
+
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    General Public License for more details.
+
+    You should have received a copy of the GNU General Public
+    License along with this library; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+
+--------------------------------------------------------------------
+
+	The latest version of strus can be found at 'http://github.com/patrickfrey/strus'
+	For documentation see 'http://patrickfrey.github.com/strus'
+
+--------------------------------------------------------------------
+*/
+#ifndef _STRUS_LVDB_BIT_OPERATIONS_HPP_INCLUDED
+#define _STRUS_LVDB_BIT_OPERATIONS_HPP_INCLUDED
+#include "strus/index.hpp"
+#include <cstdlib>
+#include <cstring>
+
+namespace strus {
+
+struct BitOperations
+{
+	static inline unsigned int bitScanReverse( const uint32_t& idx)
+	{
+#ifdef __x86_64__
+		uint32_t result; 
+		if (!idx) return 0;
+		asm(" bsr %1, %0 \n" : "=r"(result) : "r"(idx) ); 
+		return result+1;
+#else
+		uint32_t xx = idx;
+		if (!xx) return 0;
+		int ee = 1;
+		if ((xx & 0xFFff0000))   { ee +=16; xx >>=16; }
+		if ((xx & 0x0000Ff00))   { ee += 8; xx >>= 8; }
+		if ((xx & 0x000000F0))   { ee += 4; xx >>= 4; }
+		if ((xx & 0x0000000C))   { ee += 2; xx >>= 2; }
+		if ((xx & 0x00000002))   { ee += 1; }
+		return ee;
+#endif
+	}
+
+	static inline unsigned int bitScanReverse( const uint16_t& idx)
+	{
+		uint32_t xx = idx;
+		if (!xx) return 0;
+#ifdef __x86_64__
+		uint32_t result; 
+		asm(" bsr %1, %0 \n" : "=r"(result) : "r"(xx) ); 
+		return result+1;
+#else
+		int ee = 1;
+		if ((xx & 0xFf00))   { ee += 8; xx >>= 8; }
+		if ((xx & 0x00F0))   { ee += 4; xx >>= 4; }
+		if ((xx & 0x000C))   { ee += 2; xx >>= 2; }
+		if ((xx & 0x0002))   { ee += 1; }
+		return ee;
+#endif
+	}
+
+	static inline unsigned int bitScanReverse( const uint8_t& idx)
+	{
+		uint32_t xx = idx;
+		if (!xx) return 0;
+#ifdef __x86_64__
+		uint32_t result; 
+		asm(" bsr %1, %0 \n" : "=r"(result) : "r"(xx) ); 
+		return result+1;
+#else
+		int ee = 1;
+		if ((xx & 0xF0))   { ee += 4; xx >>= 4; }
+		if ((xx & 0x0C))   { ee += 2; xx >>= 2; }
+		if ((xx & 0x02))   { ee += 1; }
+		return ee;
+#endif
+	}
+
+	static inline unsigned int bitScanForward( const uint32_t& idx)
+	{
+#ifdef __x86_64__
+		uint32_t result; 
+		if (!idx) return 0;
+		asm(" bsf %1, %0 \n" : "=r"(result) : "r"(idx) ); 
+		return result+1;
+#else
+		return ffs( idx);
+#endif
+	}
+
+	static inline unsigned int bitScanForward( const uint64_t& idx)
+	{
+#ifdef __x86_64__
+		uint64_t result; 
+		if (!idx) return 0;
+		asm(" bsfq %1, %0 \n" : "=r"(result) : "r"(idx) ); 
+		return (unsigned int)(result+1);
+#else
+		return ffsl( idx);
+#endif
+	}
+};
+}//namespace
+#endif
---
+++ src/testmodule/postingIteratorHelpers.cpp
@@ -0,0 +1,179 @@
+/*
+---------------------------------------------------------------------
+    The C++ library strus implements basic operations to build
+    a search engine for structured search on unstructured data.
+
+    Copyright (C) 2015 Patrick Frey
+
+    This library is free software; you can redistribute it and/or
+    modify it under the terms of the GNU General Public
+    License as published by the Free Software Foundation; either
+    version 3 of the License, or (at your option) any later version.
+
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    General Public License for more details.
+
+    You should have received a copy of the GNU General Public
+    License along with this library; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+
+--------------------------------------------------------------------
+
+	The latest version of strus can be found at 'http://github.com/patrickfrey/strus'
+	For documentation see 'http://patrickfrey.github.com/strus'
+
+--------------------------------------------------------------------
+*/
+/// \brief Implementation of helper functions shared by iterators
+#include "postingIteratorHelpers.hpp"
+#include "strus/errorBufferInterface.hpp"
+#include "strus/private/internationalization.hpp"
+#include "strus/private/errorUtils.hpp"
+#include "strus/private/bitOperations.hpp"
+#include <sstream>
+#include <iostream>
+
+using namespace strus;
+
+Index strus::getFirstAllMatchDocno(
+		std::vector<Reference< PostingIteratorInterface> >& args,
+		Index docno,
+		bool allowEmpty)
+{
+	if (args.empty()) return 0;
+	
+	Index docno_iter = docno;
+	std::vector<Reference< PostingIteratorInterface> >::iterator
+		ae = args.end();
+	for (;;)
+	{
+		std::vector<Reference< PostingIteratorInterface> >::iterator
+			ai = args.begin();
+
+		docno_iter = (*ai)->skipDocCandidate( docno_iter);
+		if (docno_iter == 0)
+		{
+			return 0;
+		}
+		for (++ai; ai != ae; ++ai)
+		{
+			Index docno_next = (*ai)->skipDocCandidate( docno_iter);
+			if (docno_next == 0)
+			{
+				return 0;
+			}
+			if (docno_next != docno_iter)
+			{
+				docno_iter = docno_next;
+				break;
+			}
+		}
+		if (ai == ae)
+		{
+			if (!allowEmpty)
+			{
+				ai = args.begin();
+				for (; ai != ae; ++ai)
+				{
+					if (docno_iter != (*ai)->skipDoc( docno_iter))
+					{
+						++docno_iter;
+						continue;
+					}
+				}
+			}
+			return docno_iter;
+		}
+	}
+}
+
+Index strus::getFirstAllMatchDocnoSubset(
+		std::vector<Reference< PostingIteratorInterface> >& args,
+		Index docno,
+		bool allowEmpty,
+		std::size_t cardinality)
+{
+	if (args.empty()) return 0;
+	if (args.size() > 64) throw std::runtime_error("number of arguments for getFirstAllMatchDocnoSubset out of range");
+
+	Index docno_iter = docno;
+	for (;;)
+	{
+		std::vector<Reference< PostingIteratorInterface> >::iterator
+			ai = args.begin(), ae = args.end();
+
+		std::size_t nof_matches = 0;
+		Index match_docno = 0;
+		uint64_t candidate_set = 0;
+
+		for (unsigned int aidx = 0; ai != ae; ++ai,++aidx)
+		{
+			Index docno_next = (*ai)->skipDocCandidate( docno_iter);
+			if (docno_next)
+			{
+				if (match_docno)
+				{
+					if (match_docno == docno_next)
+					{
+						candidate_set |= (uint64_t)1<<(aidx);
+						++nof_matches;
+					}
+					else if (match_docno > docno_next)
+					{
+						candidate_set = (uint64_t)1<<(aidx);
+						match_docno = docno_next;
+						nof_matches = 1;
+					}
+				}
+				else
+				{
+					candidate_set = (uint64_t)1<<(aidx);
+					match_docno = docno_next;
+					nof_matches = 1;
+				}
+			}
+		}
+		if (nof_matches >= cardinality)
+		{
+			if (!allowEmpty)
+			{
+				unsigned int aidx = BitOperations::bitScanForward( candidate_set);
+				while (aidx)
+				{
+					if (match_docno != (*ai)->skipDoc( match_docno))
+					{
+						--nof_matches;
+						if (nof_matches < cardinality) break;
+					}
+					candidate_set -= (uint64_t)1<<(aidx-1);
+					aidx = BitOperations::bitScanForward( candidate_set);
+				}
+				if (nof_matches < cardinality)
+				{
+					docno_iter = match_docno+1;
+					continue;
+				}
+			}
+			return match_docno;
+		}
+		else if (match_docno)
+		{
+			docno_iter = match_docno+1;
+		}
+		else
+		{
+			break;
+		}
+	}
+	return 0;
+}
+
+void strus::encodeInteger( std::string& buf, int val)
+{
+	std::ostringstream num;
+	num << val;
+	buf.append( num.str());
+}
+
